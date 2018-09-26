3c1a391f43e1197222d6821d8f095d74dcfb1a30
sumanth8ch <chsm17@student.bth.se>
2018-09-26 22:08:04 +0200

    utf-8 used

--- client.py
+++ client.py
@@ -1,16 +1,16 @@
-import socket,sys,select
+import socket,sys,select,string
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-HOST ='18.219.51.6'
-PORT = 4711
-s.settimeout(1)
-s.connect(('18.219.51.6',4711))
+HOST =socket.gethostname()
+PORT = 4004
+s.settimeout(10)
+s.connect((HOST,PORT))
 print("Connected to server")
-name = raw_input("Enter nickname: ")
-s.send(name)
+name = input("Enter nickname: ")
+s.send(name.encode('utf-8'))
 sys.stdout.write("You: ")
 sys.stdout.flush()
 while 1 :
-	sock_list = [sys.stdin,s]
+	sock_list = [s]
 	r_list,w_list,e_list = select.select(sock_list,[], [] )
 	for i in r_list:
 		if i == s:
@@ -19,12 +19,11 @@ while 1 :
 				print("DISCONNECTED")
 				sys.exit()
 			else:
-				sys.stdout.write(msg_rcv)
+				sys.stdout.write(msg_rcv.decode('utf-8'))
 				sys.stdout.write("You: ")
 				sys.stdout.flush()
 		else:
 			msg = sys.stdin.readline()
-			s.send(msg)
+			s.send(msg.encode('utf-8'))
 			sys.stdout.write("You: ")
 			sys.stdout.flush()
-
