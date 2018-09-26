1d7d691b6c8ceed7cf66f6bc1f83a9ddebcd11c8
sumanth8ch <chsm17@student.bth.se>
2018-09-26 17:31:07 +0200

    1

---
+++ client.py
@@ -0,0 +1,30 @@
+import socket,sys,select
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+HOST ='18.219.51.6'
+PORT = 4711
+s.settimeout(0)
+s.connect(('18.219.51.6',4711))
+print("Connected to server")
+name = raw_input("Enter nickname: ")
+s.send(name)
+sys.stdout.write("You: ")
+sys.stdout.flush()
+while true :
+	sock_list = [sys.stdin,s]
+	r_list,w_list,e_list = select.select(sock_list,[], [] )
+	for i in r_list:
+		if i == s:
+			msg_rcv = i.recv(4096)
+			if not msg_rcv:
+				print("DISCONNECTED")
+				sys.exit()
+			else:
+				sys.stdout.write(msg_rcv)
+				sys.stdout.write("You: ")
+				sys.stdout.flush()
+		else:
+			msg = sys.stdin.readline()
+			s.send(msg)
+			sys.stdout.write("You: ")
+			sys.stdout.flush()
+
