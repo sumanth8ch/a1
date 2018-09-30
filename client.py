5eb00ae411d778b28541b20c89f1d12e6591f333
sumanth8ch <chsm17@student.bth.se>
2018-09-30 18:11:03 +0200

    error not able to send message has been modified

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket,sys,select,string
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 HOST =socket.gethostname()
-PORT = 4004
+PORT = 33000
 s.settimeout(10)
 s.connect((HOST,PORT))
 print("Connected to server")
@@ -10,20 +10,19 @@ s.send(name.encode('utf-8'))
 sys.stdout.write("You: ")
 sys.stdout.flush()
 while 1 :
-	sock_list = [s]
+	sock_list = [socket.socket(),s]
 	r_list,w_list,e_list = select.select(sock_list,[], [] )
 	for i in r_list:
 		if i == s:
-			msg_rcv = i.recv(4096)
+			msg_rcv = i.recv(1024)
+			print(msg_rcv)
 			if not msg_rcv:
 				print("DISCONNECTED")
 				sys.exit()
 			else:
-				sys.stdout.write(msg_rcv.decode('utf-8'))
+				msg = sys.stdin.readline()
+				s.send(msg.encode('utf-8'))
 				sys.stdout.write("You: ")
+				sys.stdout.write(msg_rcv.decode('utf-8'))
 				sys.stdout.flush()
-		else:
-			msg = sys.stdin.readline()
-			s.send(msg.encode('utf-8'))
-			sys.stdout.write("You: ")
-			sys.stdout.flush()
+s.close()
